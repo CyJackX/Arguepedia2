4352053cddbb9b4acf8023478f6cd1c767998886
CyJackX <42789314+CyJackX@users.noreply.github.com>
2025-04-02 21:56:05 -0400

    Card Styling

--- src/components/StatementComponent.vue
+++ src/components/StatementComponent.vue
@@ -15,28 +15,30 @@ defineProps<{
 </script>
 
 <template>
-  <q-item clickable @click="() => router.push(`/statement/${statement.id}`)" :inset-level="insetLevel">
-    <q-item-section side class="text-caption" v-if="sideStats">
-      <q-item-label>
-        <q-icon color="green" name="check" /> {{ statement.supporting_arguments_count }}
-      </q-item-label>
-      <q-item-label>
-        <q-icon color="red" name="close" /> {{ statement.opposing_arguments_count }}
-      </q-item-label>
-      <q-item-label>
-        <q-icon name="comment" /> {{ statement.comments_count }}
-      </q-item-label>
-    </q-item-section>
-    <q-item-section>
-      <q-item-label :style="{ fontWeight: bold ? 'bold' : 'normal' }">{{ statement.statement_text }}</q-item-label>
+  <q-card class="q-mb-sm">
+    <q-item clickable @click="() => router.push(`/statement/${statement.id}`)" :inset-level="insetLevel">
+      <q-item-section side class="text-caption" v-if="sideStats">
+        <q-item-label>
+          <q-icon color="green" name="check" /> {{ statement.supporting_arguments_count }}
+        </q-item-label>
+        <q-item-label>
+          <q-icon color="red" name="close" /> {{ statement.opposing_arguments_count }}
+        </q-item-label>
+        <q-item-label>
+          <q-icon name="comment" /> {{ statement.comments_count }}
+        </q-item-label>
+      </q-item-section>
+      <q-item-section>
+        <q-item-label :style="{ fontWeight: bold ? 'bold' : 'normal' }">{{ statement.statement_text }}</q-item-label>
 
-      <q-item-label caption v-if="bottomStats">
-        <q-icon color="green" name="check" /> {{ statement.supporting_arguments_count }} | <q-icon color="red"
-          name="close" />
-        {{ statement.opposing_arguments_count }} | <q-icon name="comment" /> {{ statement.comments_count }}
-        <br>Created by
-        <UsernameButton :username="statement.username" /> on {{ new Date(statement.created_at).toLocaleString() }}
-      </q-item-label>
-    </q-item-section>
-  </q-item>
+        <q-item-label caption v-if="bottomStats">
+          <q-icon color="green" name="check" /> {{ statement.supporting_arguments_count }} | <q-icon color="red"
+            name="close" />
+          {{ statement.opposing_arguments_count }} | <q-icon name="comment" /> {{ statement.comments_count }}
+          <br>Created by
+          <UsernameButton :username="statement.username" /> on {{ new Date(statement.created_at).toLocaleString() }}
+        </q-item-label>
+      </q-item-section>
+    </q-item>
+  </q-card>
 </template>
