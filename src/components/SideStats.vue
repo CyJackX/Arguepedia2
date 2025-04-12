542c0716008fa6074b896dbd38c669814a9fda85
CyJackX <42789314+CyJackX@users.noreply.github.com>
2025-04-11 21:49:28 -0400

    refactor

---
+++ src/components/SideStats.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps<{
+  supporting_arguments_count: number;
+  opposing_arguments_count: number;
+  comments_count: number;
+}>();
+
+// Disable attribute inheritance on root element if needed
+defineOptions({
+  inheritAttrs: false
+});
+
+</script>
+<template>
+  <q-item-section v-bind="$attrs">
+    <q-item-label>
+      <q-icon color="green" name="check" /> {{ props.supporting_arguments_count || 0 }}
+    </q-item-label>
+    <q-item-label>
+      <q-icon color="red" name="close" /> {{ props.opposing_arguments_count || 0 }}
+    </q-item-label>
+    <q-item-label>
+      <q-icon name="comment" /> {{ props.comments_count || 0 }}
+    </q-item-label>
+  </q-item-section>
+</template>
